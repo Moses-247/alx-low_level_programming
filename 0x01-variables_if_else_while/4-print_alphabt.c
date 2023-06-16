1510f8257c4a417b6b98e22c2f275fe85cc48023
Moses-247 <71809408+Moses-247@users.noreply.github.com>
2023-06-16 16:27:38 +0300

    0x01

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,9 +4,7 @@
 /**
  * main - Prints the alphabet without q and e.
  *
-
  * Return: Always 0 (Success)
-
  */
 int main(void)
 {
@@ -16,11 +14,8 @@ int main(void)
                 if (i != 101 && i != 113)
                 {
                         putchar(i);
-
                 }
-
         }
-
         putchar('\n');
         return (0);
 }
