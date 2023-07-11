cd42a5be34a392187bbd1ec84e8a06d746c87e06
Moses Asuga <asugam9@gmail.com>
2023-07-11 13:07:48 -0700

    0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,28 +1,16 @@
-#include "main.h"
-		
-#include <stdlib.h>
-		
-/**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
-		
- * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-		
+#include "main.h"		
+#include <stdlib.h>		
+/**		
+ * str_concat - get ends of input and add together for size	
+ * @s1: input one to concat		
+ * @s2: input two to concat		
+ * Return: concat of s1 and s2		
  */
 		
 char *str_concat(char *s1, char *s2)
-		
-{
-		
-	char *conct;
-		
+{		
+	char *conct;	
 	int i, ci;
-		
-
 		
 	if (s1 == NULL)
 		
@@ -63,21 +51,11 @@ char *str_concat(char *s1, char *s2)
 		i++;
 		
 	}
-		
-
-		
-	while (s2[ci] != '\0')
-		
+	while (s2[ci] != '\0')	
 	{
-		
 		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
+		i++, ci++;	
 	}
-		
 	conct[i] = '\0';
-		
-	return (conct);
-		
+	return (conct);		
 }
